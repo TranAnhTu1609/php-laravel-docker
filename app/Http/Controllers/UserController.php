99f0711d0bfabcd00a3d4016e2a17436b9ae598c
TranAnhTu <tuta@deha-soft.com>
2024-07-15 17:07:31 +0700

    handle_logic_users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,8 +9,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::get();
-
-        return view('users.users', compact('users'));
+       return 1;
     }
 }
